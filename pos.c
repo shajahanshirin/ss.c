7b7a698274c6d5a481922d11551f081940971ed3
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-01-08 16:44:16 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number");
+scanf("%d",num);
+if(num>0)
+{
+printf("positive number");
+}
+else
+{
+printf("negative number");
+}
+return o;
+}
