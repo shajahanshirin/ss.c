a8e3bd0ae30a742b8d69d9db707b33199b40489c
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-01-13 11:15:01 +0530

    Create alp.c

---
+++ alp.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char ch:
+printf("enter the character");
+scanf("%c",ch);
+if(ch>=92&&ch>=65&&ch>=97&&ch>=105)
+{
+printf("alpahabet");
+}
+else
+{
+printf("not a alphabet");
+}
+}
