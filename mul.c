c27c8d7aaa9056e9ea0c784c7aa730be6e31e697
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-01-16 12:08:14 +0530

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+        int sum=0,i,n;
+        clrscr();
+        printf("\n Please Give The Value of N:  ");
+        scanf("%ld",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("\n 5 * %ld = %ld",i,5*i);
+        }
+        }
+        
