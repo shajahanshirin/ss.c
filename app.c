70a99446cd0eea4220c7c3d175bd9fdff025da2e
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-01-21 10:59:57 +0530

    Create app.c

---
+++ app.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,d,n,sum=1,i;
+printf("enter number");
+scanf("%d%d%d",&n,&a,%d);
+for(i=n;i<n;i--)
+{
+sum=a+(n-1)d;
+}
+sum=sum+a;
+printf("sum:%d",sum);
+}
