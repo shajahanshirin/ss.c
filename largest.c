9097d5a1a3e1c39860007acb30e237daec921a8b
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-01-24 16:56:19 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a[20]={1,2,3,4,5,6,7,8,9,10},i,max;
+	for(i=0;i<a[i];i++)
+	{
+		if(a[i]>max)
+		{
+			max=a[i];
+		
+		}
+	}
+printf("largest number:%d",max);
+return 0;
+}
