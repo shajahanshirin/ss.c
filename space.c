d785dcb8f9217249e42f9337d495b52012cec141
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-01-31 11:10:03 +0000

    Create space.c

---
+++ space.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  int a,i,sum=0;
+  char str[40];
+  printf("enter the string");
+  scanf("%s",&str);
+  a=strlen(str);
+  for(i=0;i<=a;i++)
+  {
+    if(str[i]==' ')
+    {
+      sum=sum+1;
+    }
+  }
+  printf("%d",sum);
+  return 0;
+}
