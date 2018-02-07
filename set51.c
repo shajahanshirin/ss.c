38298fd1485a86820799170bf27d3c2f255e8001
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-02-07 15:59:37 +0530

    Create set51.c

---
+++ set51.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char a[50];
+	int n,i;
+	printf("\nenter the string");
+	scanf("%s",&a);
+	printf("\nenter the number");
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+		printf("%s\n",a);
+	}
+return 0;	
+}
