b0417f37903fc76f6303e4f723f93a6f7f8d5b2f
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-02-09 11:23:13 +0000

    Rename word.c to words.c

---
+++ words.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n,a;
+printf("enter choice:");
+scanf("%d",&n);
+printf(enter number");
+scanf("%d",&a);
+switch(n)
+{
+case 1:
+{
+printf("one");
+break;
+}
+case 2:
+{
+printf("two");
+break;
+}
+}
+return 0;
+}
