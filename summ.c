103694e1f5c8d871778297bcf98bee29c6c0bd51
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-02-09 10:17:58 +0000

    Create summ.c

---
+++ summ.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+printf("enter frst number");
+scanf("%d",&a);
+printf("enter second number");
+scanf("%d",&b);
+sum=a+b;
+printf("sum is:%d",sum);
+return 0;
+}
