179b1919c2e24e9d2bb3388a7f109d02e1fb7c30
shajahanshirin <35136564+shajahanshirin@users.noreply.github.com>
2018-02-09 11:10:25 +0000

    Create splchar.c

---
+++ splchar.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+char a[100];
+int count;
+int c=0;
+printf("enter the string");
+scanf("%s",&a);
+if(strlen(a)<100000)
+{
+for(count=0;count!=null;count++)
+{
+if(a[count]>='0'&&a[count]<='9')
+{
+}
+else if((a[count]>='a'&&a[count]<='z')||(a[count]<='A'&&a[count]>='z'))
+{
+}
+else if(a[count==' ')
+{
+}
+else
+{
+c++;
+}
+printf("%d",c);
+return 0;
+}
